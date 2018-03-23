61a14d5671a8663fefa89f112fd1f7e4af8ed897
johanh <johanhagel@hotmail.com>
2018-03-23 11:25:54 +0100

    Nu ska följande inte finnas längre: modifierat lite i gitkladd.c

--- gitkladd.c
+++ gitkladd.c
@@ -4,4 +4,8 @@ Adding some text
 Skriver lite mer här nu...
 
 Addade det på rad 4, o nu skriver lite till igen...
+
+
+Nu skriver jag mer igen.
+FAn, detta skulle ju också var amed i committen!
 */
